733e1f8da393601a4a3a3838355753265f387d34
Andrew <telepnev.1997@mail.ru>
2024-02-10 23:50:40 +0700

    added working with mysql

--- src/main/java/com/example/GB_SpringCore_LK4/repository/UserRepository.java
+++ src/main/java/com/example/GB_SpringCore_LK4/repository/UserRepository.java
@@ -2,6 +2,7 @@ package com.example.GB_SpringCore_LK4.repository;
 
 
 import com.example.GB_SpringCore_LK4.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
@@ -11,23 +12,6 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicLong;
 
 @Repository
-public class UserRepository {
-    private Map<Long, User> users = new ConcurrentHashMap<>();
-    private AtomicLong counter = new AtomicLong();
+public interface UserRepository extends JpaRepository<User, Long> {
 
-    public List<User> findAll() {
-        return new ArrayList<>(users.values());
-    }
-
-    public User findById(Long id) {
-        return users.get(id);
-    }
-
-    public User save(User user) {
-        if (user.getId() == null) {
-            user.setId(counter.incrementAndGet());
-        }
-        users.put(user.getId(), user);
-        return user;
-    }
 }
